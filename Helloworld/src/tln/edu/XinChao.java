b0befb9930797b3a72cfcda7e250551f009982b1
Võ Tấn Thành <thanh.vt.63cntt@ntu.edu.vn>
2024-01-23 18:45:24 +0700

    demo

---
+++ Helloworld/src/tln/edu/XinChao.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package tln.edu;
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("tôi lần đầu học java và đang thử nghiệm");
+	}
+
+}
