416d81e5462e507418b1294d11168792ec7a30a8
Võ Tấn Thành <thanh.vt.63cntt@ntu.edu.vn>
2025-02-12 23:53:25 +0700

    helloword

---
+++ Bai1_Hello/src/SayHelloApp.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class SayHelloApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Xin chao, em la Vo Tan Thanh SV K63CNTT - NTU: Hoc lap trinh Java");
+	}
+
+}
